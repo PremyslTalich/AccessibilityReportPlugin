bbece70980cb85e4f765a3f02b5538ba9706baf7
Přemysl Talich <p.talich@gmail.com>
2026-07-29 00:40:29 +0200

    Bump version to 1.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ fun localProperty(name: String): String? =
     localProperties.getProperty(name)
 
 group = "cz.talich.arp"
-version = "1.3.1"
+version = "1.4.0"
 
 repositories {
     mavenCentral()
